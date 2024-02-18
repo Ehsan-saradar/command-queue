ceed627e0552506343ea829ce3a0baa31a59faa2
ehsan <ehsan.saradar@gmail.com>
2024-02-18 12:02:16 +0330

    update console logger

--- internal/util/logger/consolelogger.go
+++ internal/util/logger/consolelogger.go
@@ -2,13 +2,10 @@ package logger
 
 import (
 	"fmt"
-	"sync"
 )
 
 // ConsoleLogger implements the Logger interface for logging to the console.
-type ConsoleLogger struct {
-	mu sync.Mutex // Mutex for synchronization
-}
+type ConsoleLogger struct{}
 
 // NewConsoleLogger creates a new instance of ConsoleLogger.
 func NewConsoleLogger() Logger {
@@ -17,7 +14,5 @@ func NewConsoleLogger() Logger {
 
 // Logf logs a formatted message to the console.
 func (c *ConsoleLogger) Logf(format string, args ...interface{}) {
-	c.mu.Lock()         // Acquire mutex lock to ensure exclusive access to the console
-	defer c.mu.Unlock() // Release mutex lock when Logf exits, even in case of panics or early returns
 	fmt.Printf(format, args...)
 }
